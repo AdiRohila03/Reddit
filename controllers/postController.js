02045a3cac819bc29bddb45474a5394aa4eef685
Aditya <adirohila03@gmail.com>
2022-11-10 17:55:51 +0530

    Authenticated the controller for a particula user

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,67 +1,96 @@
-const Post = require('../model/postModel');   
-const User = require('../model/userModel'); 
+const Post = require('../model/postModel');
+const User = require('../model/userModel');
 
-const create = async (req, res) => {           //Add post's data to DB
-    const user = await User.findOne({_id: req.params._id});      //Stores user_id from params
-
-    const data = new Post(req.body);                           //Takes input from postman body
-    data.user = user._id;                            //Adds user_id to the post
+const create = async (req, res) => {                              //Create Post
+    try {
+    //Checks if the entered user_id is present if true stores user details in 'user'
+    const user = await User.findOne({ _id: req.params._id });     
+    const data = new Post(req.body);                             //Stores Post details from body
+    data.user = user._id;                                        //Adds user_id to the post schema
     await data.save();
-    res.send("Post Created:");      
+    res.status(200).send({ success: true, Details: data });
+    } catch (err) {
+        console.log(err);
+        res.status(400).send({ success: false, message: err });    
+    }    
 }
 
-const get = async (req, res) => {             //Displays all posts 
-    let data = await Post.find();
-    res.send(data);
+const get = async (req, res) => {                               //Displays posts 
+    let post = await Post.find();
+    res.status(200).send({ success: true, Posts: post });
 }
+
+/*const personal = async (req, res) => {                               //Displays  posts of particular user 
+    const user = await User.findOne({ email: req.body.email });    // to restrict the user to see only user's post
+    let post = await Post.find();
+    res.status(200).send({ success: true, message: post });
+}*/
 
 const remove = async (req, res) => {          //Removes a paricular post 
     try {
-        let data = await Post.findByIdAndDelete(req.params);
-        res.send("Post Deleted");
+        let data = await Post.findOne(req.params);         //Saves Post details to data after verifying post_id
+        let postId = data.user.toString();
+        let user = await User.findOne({ email: req.data.email });
+        let userId = user._id.toString();
+        if (postId === userId) {
 
-    } catch (error) {
-        res.send("Id Not Found");
-        console.log(err.message);
-    };
+            await Post.findOneAndDelete(data);
+            res.status(200).send({ success: true, message: "Post Deleted" });
+        } else {
+            res.status(200).send({ success: false, message: "Invalid User Details" });
+        }
+    }
+    catch (err) {
+        console.log(err);
+        res.status(400).send({ success: false, message: err });
+    }
 
 }
 
-const update = async (req, res) => {          //Update/Change post details
+const update = async (req, res) => {          //Update post details
     try {
-        let data = await Post.findByIdAndUpdate(req.params,      //Condition for update(post_id in this case given in routes)
+        let data = await Post.findOne(req.params);       //Condition for update(post_id in this case given in routes)
+        let postId = data.user.toString();
+        let user = await User.findOne({ email: req.data.email });
+        let userId = user._id.toString();
+        if (postId === userId) {
+            await Post.findByIdAndUpdate(data,
             {
                 $set: req.body                                 //Updated Value
-            }
-        );
-        res.send(data);
-    } catch (error) {
-        res.send("Id Not Found");
-        console.log(err.message);
+            });
+            res.status(200).send({ success: true, message: "Post Updated", Updated_Post: data });
+        } else {
+            res.status(200).send({ success: false, message: "Invalid User Details" });
+        }
+        } catch (err) {
+        console.log(err);
+        res.status(400).send({success: false, message:err});        
     }
 
 }
 
-const likes = async (req, res) => {               
-try {
-    const post = await Post.findOne({_id: req.params._id});  //Finds post using post_id from params
-    post.likes ++;                                          //Increases Like counter
-    await post.save();
-    res.send(post);
-    }catch(err){
-        res.send("No Post Found!");
-}
-}
-
-const dislikes = async (req, res) => {   
+const likes = async (req, res) => {
     try {
-        const post = await Post.findOne({_id: req.params._id});
-        post.dislikes ++; 
+        const post = await Post.findOne({ _id: req.params._id });  //Finds post using post_id from params
+        post.likes++;                                             //Increases Like counter
         await post.save();
-        res.send(post);
-        }catch(err){
-            res.send("No Post Found!");
+        res.status(200).send(post);
+    } catch (err) {
+        console.log(err);
+        res.status(400).send("No Post Found!");
     }
-    }
+}
 
-module.exports = { create,get,remove,update,likes,dislikes }
+const dislikes = async (req, res) => {
+    try {
+        const post = await Post.findOne({ _id: req.params._id });
+        post.dislikes++;
+        await post.save();
+        res.status(200).send(post);
+    } catch (err) {
+        console.log(err);
+        res.status(400).send("No Post Found!");
+    }
+}
+
+module.exports = { create, get, remove, update, likes, dislikes }
