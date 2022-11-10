a251e362b961ecb2127ec02757cc75503cc537b2
Aditya <adirohila03@gmail.com>
2022-11-10 17:58:38 +0530

    Added login api for authentication,generated jwt token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,55 @@
-const User = require('../model/userModel');   
+const User = require('../model/userModel');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+//Method to create JWT Token
+const creat_token = async (Email) => {
+    try {
+        const token = await jwt.sign({ email: Email }, process.env.KEY);  //Creates JWT Token using Email as params
+        return token;
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+}
+
+//Authentication via email paired with password
+const login = async (req, res) => {
+    try {
+        //Checks if the entered email is present if true saves user details in 'data'
+        const data = await User.findOne({ email: req.body.email });
+        if (data) {
+            //Checks if the password enterd is correct
+            const pass = await bcrypt.compare(req.body.password, data.password);
+            if (pass) {
+                const tokendata = await creat_token(req.body.email);     //Calls create_token method
+                res.status(200).send({ success: true, messsage: "Succesfully Logged In Token generated: ", token: tokendata });
+            }
+            else {
+                res.status(200).send({ success: false, msg: "Login details are Incorrect" });
+            }
+
+        } else {
+            res.status(200).send({ success: false, msg: "Login details are Incorrect" });
+
+        }
+
+    } catch (err) {
+        console.log(err);
+        res.status(400).send({ success: false, messsage: err });
+    }
+
+}
 
 const create = async (req, res) => {           //Adds user details to DB 
     let data = new User(req.body);
-    let result = await data.save();
-    res.send("Data Inserted");
+    await data.save();
+    res.status(200).send({ success: true, message: "Data Inserted" });
 }
 
 const get = async (req, res) => {             //Displays user details 
-    let data = await User.find();
-    res.send(data);
+    let data = await User.findOne({ email: req.data.email });
+    res.status(200).send(data);
 }
 
 const remove = async (req, res) => {          //Removes a paricular user
@@ -31,11 +72,11 @@ const update = async (req, res) => {          //Update/Change data
             }
         );
         res.send(data);
-    } catch (error) {
+    } catch (err) {
         res.send("Id Not Found");
         console.log(err.message);
     }
 
 }
 
-module.exports = { create,get,remove,update }
+module.exports = { login, create, get, remove, update }
