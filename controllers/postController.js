aab66204f6fa843f996ab519b28cfb1acadfe68f
Aditya <adirohila03@gmail.com>
2023-04-05 23:16:08 +0530

    Added new features to User/Post Controllers

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,103 +1,150 @@
-const Post = require('../model/postModel');
-const User = require('../model/userModel');
+const postSchema = require('../models/postModel');
+const userSchema = require('../models/userModel');
 
-const create = async (req, res) => {                              //Create Post
-    try {
-    //Checks if the entered user_id is present if true stores user details in 'user'
-    const user = await User.findOne({ _id: req.params._id });     
-    const data = new Post(req.body);                             //Stores Post details from body
-    const file = req.files;
-    console.log(file);
-    //Conversion of img files to Buffer
-    for(var i=0 ; i < file.length ; i++)
-    {
-        data.files[i] = file[i].buffer;
-    }
-    data.user = user._id;                                        //Adds user_id to the post schema
-    await data.save();
-    res.status(200).send({ success: true, Details: data });
-    } catch (err) {
-        console.log(err);
-        res.status(400).send({ success: false, message: err });    
-    }    
-}
-
-const get = async (req, res) => {                               //Displays posts 
-    let post = await Post.find().select("-files");
-    res.status(200).send({ success: true, Posts: post });
-}
-
-/*const personal = async (req, res) => {                               //Displays  posts of particular user 
-    const user = await User.findOne({ email: req.body.email });    // to restrict the user to see only user's post
-    let post = await Post.find();
-    res.status(200).send({ success: true, message: post });
-}*/
-
-const remove = async (req, res) => {          //Removes a paricular post 
-    try {
-        let data = await Post.findOne(req.params);         //Saves Post details to data after verifying post_id
-        let postId = data.user.toString();
-        let user = await User.findOne({ email: req.data.email });
-        let userId = user._id.toString();
-        if (postId === userId) {
-
-            await Post.findOneAndDelete(data);
-            res.status(200).send({ success: true, message: "Post Deleted" });
-        } else {
-            res.status(200).send({ success: false, message: "Invalid User Details" });
-        }
-    }
-    catch (err) {
-        console.log(err);
-        res.status(400).send({ success: false, message: err });
-    }
-
-}
-
-const update = async (req, res) => {          //Update post details
-    try {
-        let data = await Post.findOne(req.params);       //Condition for update(post_id in this case given in routes)
-        let postId = data.user.toString();
-        let user = await User.findOne({ email: req.data.email });
-        let userId = user._id.toString();
-        if (postId === userId) {
-            await Post.findByIdAndUpdate(data,
-            {
-                $set: req.body                                 //Updated Value
+//CREATE
+const create = async (req, res) => {
+    if (req.user.id === req.params._id || req.user.isAdmin) {
+        try {
+            const postUser = await userSchema.findOne({ _id: req.params._id });
+            const data = new postSchema(req.body);                             //Stores Post details from body
+            data.user = postUser._id;                                        //Adds user_id to the post schema
+            await data.save().then(() =>
+                res.status(201).send({ message: "Post Created", Post: data })
+            ).catch((err) => {
+                res.status(500).json(err);
+                console.log(err);
             });
-            res.status(200).send({ success: true, message: "Post Updated", Updated_Post: data });
-        } else {
-            res.status(200).send({ success: false, message: "Invalid User Details" });
-        }
         } catch (err) {
-        console.log(err);
-        res.status(400).send({success: false, message:err});        
+            res.status(500).json(err);
+            console.log(err);
+        }
+    }
+    else {
+        res.status(403).json("Allowed to add post only to your account");
+    }
+}
+
+//UPDATE
+const update = async (req, res) => {
+    let data = await postSchema.findById(req.params._id);
+    let userId = data.user.toString();
+    if (req.user.id === userId || req.user.isAdmin) {
+        try {
+            await data.updateOne(
+                {
+                    $set: req.body
+                });
+            res.status(200).send({ success: true, message: "Post Updated", Updated_Post: data });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    }
+    else {
+        res.status(403).json("Allowed to update only your post");
+    }
+}
+
+//DELETE
+const remove = async (req, res) => {
+    try {
+        const data = await postSchema.findById(req.params._id);
+        const userId = data.user.toString();
+        if (req.user.id === userId || req.user.isAdmin) {
+            try {
+                await postSchema.findByIdAndDelete(req.params._id);
+                res.status(200).send({ success: true, message: "Post Deleted" });
+            } catch (err) {
+                console.log(err);
+                res.status(500).json(err);
+            }
+        }
+        else {
+            res.status(403).json("Allowed to delete only your post");
+        }
+    } catch (err) {
+        res.status(500).json({ message: "Post Not Found", Error: err });
     }
 
 }
 
+//LIKE
 const likes = async (req, res) => {
     try {
-        const post = await Post.findOne({ _id: req.params._id });  //Finds post using post_id from params
-        post.likes++;                                             //Increases Like counter
-        await post.save();
-        res.status(200).send(post);
+        const post = await postSchema.findById(req.params._id);
+        if (post) {
+            if (!post.likes.includes(req.user.id)) {
+                await post.updateOne({ $push: { likes: req.user.id } });
+                res.status(200).json(`Post Liked`);
+            } else {
+                res.status(403).json(`Already Liked`);
+            }
+        } else {
+            res.status(500).send("Post Not Found");
+        }
     } catch (err) {
-        console.log(err);
-        res.status(400).send("No Post Found!");
+        res.status(500).json(err);
     }
 }
 
+//DISLIKE
 const dislikes = async (req, res) => {
     try {
-        const post = await Post.findOne({ _id: req.params._id });
-        post.dislikes++;
-        await post.save();
-        res.status(200).send(post);
+        const post = await postSchema.findById(req.params._id);
+        if (post) {
+            if (post.likes.includes(req.user.id)) {
+                await post.updateOne({ $pull: { likes: req.user.id } });
+                res.status(200).json(`Post Disliked`);
+            } else {
+                res.status(403).json(`Post not liked`);
+            }
+        } else {
+            res.status(500).send("Post Not Found");
+        }
     } catch (err) {
-        console.log(err);
-        res.status(400).send("No Post Found!");
+        res.status(500).json(err);
     }
 }
 
-module.exports = { create, get, remove, update, likes, dislikes }
+//GET
+const get = async (req, res) => {
+    try {
+        const post = await postSchema.findById(req.params._id);
+        res.status(200).send({ success: true, Post: post });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+//GET USRE'S POSTS
+const all = async (req, res) => {
+    try {
+        const currentUser = await userSchema.findById(req.user.id);
+        const userPosts = await postSchema.find({ user: currentUser._id });
+        res.status(200).send({ success: true, Post: userPosts });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+//POST TIMELINE
+const timeline = async (req, res) => {
+    try {
+        const currentUser = await userSchema.findById(req.user.id);
+        const userPosts = await postSchema.find({ user: currentUser._id });
+        const friendPosts = await Promise.all(
+            //Promise.all() to fetch posts from all the friends of a current user.
+            //Promise.all() allows you to wait for multiple promises to be resolved at the same time and get the results back as an array.
+            currentUser.following.map((friendId) => {
+                //map() method is used to loop through the array and return an array of promises.
+                //map() method allows you to take each item in the array, do something with it, and store the results in a new array.
+                return postSchema.find({ user: friendId });
+            })
+        );
+        res.status(200).json(userPosts.concat(...friendPosts));
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+module.exports = { create, update, remove, likes, dislikes, get, all, timeline }
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,108 +1,233 @@
-const User = require('../model/userModel');
+const userSchema = require('../models/userModel');
+require('dotenv').config();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
-const upload = require('../middlewares/upload');
 
-//Method to create JWT Token
-const creat_token = async (Email) => {
-    try {
-        const token = await jwt.sign({ email: Email }, process.env.KEY);  //Creates JWT Token using Email as params
-        return token;
-    } catch (err) {
-        res.status(400).send(err.message);
-    }
+//REGISTRATION
+const reg = async (req, res) => {
+    const data = new userSchema({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        mob: req.body.mob,
+        dob: req.body.dob,
+        bio: req.body.bio,
+        pf: {
+            data: req.file.buffer
+        }
+    });
+    data.save().then(() =>
+        res.status(201).send({ message: "Registration Successful" })
+    ).catch((err) => {
+        console.log(err);
+        res.status(500).json({ message: err })
+    });
 }
 
+//LOGIN
 //Authentication via email paired with password
 const login = async (req, res) => {
     try {
-        //Checks if the entered email is present if true saves user details in 'data'
-        const data = await User.findOne({ email: req.body.email });
+        //Checks if the entered email is present, if true saves user details in 'data'
+        const data = await userSchema.findOne({ email: req.body.email });
         if (data) {
             //Checks if the password enterd is correct
             const pass = await bcrypt.compare(req.body.password, data.password);
             if (pass) {
-                const tokendata = await creat_token(req.body.email);     //Calls create_token method
-                res.status(200).send({ success: true, messsage: "Succesfully Logged In Token generated: ", token: tokendata });
+                try {
+                    const tokendata = jwt.sign(
+                        { email: data.email, isAdmin: data.isAdmin, id: data._id },  //Creates JWT Token using email, isAdmin & user_id as payload
+                        process.env.KEY,
+                        { expiresIn: "2d" });
+                    res.status(200).send({ success: true, messsage: "Succesfully Logged In & Token generated: ", token: tokendata });
+                } catch (err) {
+                    res.status(400).json(err);
+                }
             }
             else {
-                res.status(200).send({ success: false, msg: "Login details are Incorrect" });
+                res.status(401).send({ success: false, msg: "Login details are Incorrect!" });
             }
-
         } else {
-            res.status(200).send({ success: false, msg: "Login details are Incorrect" });
+            res.status(401).send({ success: false, msg: "Login details are Incorrect!!" });
 
         }
+    } catch (err) {
+        res.status(400).json(err);
+    }
+}
 
+//UPDATE
+const update = async (req, res) => {
+    if (req.user.id === req.params._id || req.user.isAdmin) {
+        if (req.body.password) {                                                //If pass is updated it needs to be hashed again
+            req.body.password = await bcrypt.hash(req.body.password, 10);
+        }
+        try {
+            let data = await userSchema.findByIdAndUpdate(req.params,      //Condition for update
+                {
+                    $set: req.body                                 //Updated Value
+                },
+                { new: true }             // To display updated value on postman instantly
+            );
+            res.status(200).send(data);
+        } catch (err) {
+            res.status(500).json(err);
+            console.log(err.message);
+        }
+    } else {
+        res.status(403).json("Allowed to update only your account ")
+    }
+}
+
+//DELETE
+const remove = async (req, res) => {
+    if (req.user.id === req.params._id || req.user.isAdmin) {
+        try {
+            let data = await userSchema.findByIdAndDelete(req.params);
+            if (data)
+                res.status(200).json("User Deleted");
+            else
+                res.status(500).send("Id Not Found")
+        } catch (err) {
+            res.status(500).json(err);
+        };
+    } else {
+        res.status(403).json("Allowed to delete only your account ")
+    }
+}
+
+//GET USER PROFILE
+const user = async (req, res) => {
+    const userId = await req.query.userId;
+    const username = await req.query.username;
+    try {
+        let user = userId ? await useruserSchemaschema.findById(userId) : await userSchema.findOne({ username: username });
+        if (user) {
+            const { password, ...info } = user._doc;    //Ignores pass while displaying user details
+            res.status(200).send(info);
+        } else
+            res.status(500).send(`No User Found`);
     } catch (err) {
         console.log(err);
-        res.status(400).send({ success: false, messsage: err });
-    }
-
-}
-
-/*const create = async (req, res) => {           //Adds user details to DB 
-    let data = new User(req.body);
-    await data.save();
-    res.status(200).send({ success: true, message: "Data Inserted" });
-}*/
-
-const get = async (req, res) => {             //Displays user details 
-    let data = await User.findOne({ email: req.data.email });
-    res.status(200).send(data);
-}
-
-const remove = async (req, res) => {          //Removes a paricular user
-    try {
-        let data = await User.findByIdAndDelete(req.params);
-        res.send("User Deleted");
-
-    } catch (error) {
-        res.send("Id Not Found");
-        console.log(err.message);
+        res.status(500).json(err);
     };
-
 }
 
-const update = async (req, res) => {          //Update/Change data
-    try {
-        let data = await User.findByIdAndUpdate(req.params,      //Condition for update
-            {
-                $set: req.body                                 //Updated Value
-            }
-        );
-        res.send(data);
-    } catch (err) {
-        res.send("Id Not Found");
-        console.log(err.message);
-    }
-
-}
-
-const create = async (req, res) => {
-    upload(req, res, (err) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            const data = new User(req.body);
-            /*({
-                username: req.body.name,
-                email: req.body.name,
-                password: req.body.name,
-                mob: req.body.name,
-                dob: req.body.name,
-                bio: req.body.name,
-                pf:{
-                    data: req.file.filename
-                }
-            });*/
-            data.save().then(()=>
-            res.status(200).send({ success: true, message: "User Created" })
-            );
+//GET ALL 
+const get = async (req, res) => {
+    const query = req.query.new;
+    if (req.user.isAdmin) {
+        try {
+            let users = query ? await userSchema.find().sort({ _id: -1 }).limit(2) : await userSchema.find();
+            res.status(200).json(users);
+        } catch (err) {
+            res.status(500).json(err);
         };
-    });
-};
+    } else {
+        res.status(403).json("Not allowed to see all users");
+    }
+}
 
-module.exports = { login, create, get, remove, update } 
+//GET FRIENDS
+const friends = async (req, res) => {
+    try {
+        const user = await userSchema.findById(req.user.id)
+        const friends = await Promise.all(
+            user.following.map((friendId) => {
+                return userSchema.findById(friendId);
+            })
+        );
+        let friendList = [];
+        friends.map((friend) => {
+            const { _id, username, pf } = friend;
+            friendList.push({ _id, username, pf });
+        });
+        res.status(200).json(friendList);
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err);
+    }
+}
+
+//GET STATS
+const stats = async (req, res) => {
+    try {
+        const data = await userSchema.aggregate([
+            {
+                $project: {                         //Projects the month & year field from the createdAt field of each document.
+                    year: { $year: "$createdAt" }, // Extract year from createdAt field
+                    month: { $month: "$createdAt" } // Return 1 for Jan, 2 for Feb, ...
+                }
+            }, {
+                $group: {                //Groups the doc by year & month
+                    _id: { year: "$year", month: "$month" }, // Group by year and month
+                    total: { $sum: 1 } // Returns total users per month
+                }
+            },
+            {
+                $project: {
+                    _id: 0,               // Specifies to exclude the _id field from the output
+                    year: "$_id.year",    // Extracts the year field from the _id field and renames it as year.
+                    month: "$_id.month",
+                    total: 1              // Specifies to include the total field in the output.
+                }
+            }
+        ]);
+        res.status(200).json(data);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+//FOLLOW
+const follow = async (req, res) => {
+    if (req.user.id !== req.params._id) {
+        try {
+            const user = await userSchema.findById(req.params._id);
+            if (user) {
+                const name = user.username;
+                const currentUser = await userSchema.findById(req.user.id);
+                if (!user.followers.includes(req.user.id)) {
+                    await user.updateOne({ $push: { followers: req.user.id } });
+                    await currentUser.updateOne({ $push: { following: req.params._id } });
+                    res.status(200).json(`Following ${name}`);
+                } else {
+                    res.status(403).json(`Already following ${name}`);
+                }
+            } else {
+                res.status(500).send("Id Not Found");
+            }
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else
+        res.status(403).json("You can't follow yourself");
+}
+
+//UNFOLLOW
+const unfollow = async (req, res) => {
+    if (req.user.id !== req.params._id) {
+        try {
+            const user = await userSchema.findById(req.params._id);
+            if (user) {
+                const name = user.username;
+                const currentUser = await userSchema.findById(req.user.id);
+                if (user.followers.includes(req.user.id)) {
+                    await user.updateOne({ $pull: { followers: req.user.id } });
+                    await currentUser.updateOne({ $pull: { following: req.params._id } });
+                    res.status(200).json(`Unfollowed ${name}`);
+                } else {
+                    res.status(403).json(`Not following ${name}`);
+                }
+            } else {
+                res.status(500).send("Id Not Found");
+            }
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else
+        res.status(403).json("You can't follow yourself");
+}
+
+module.exports = { reg, login, update, get, remove, user, stats, follow, unfollow, friends }
